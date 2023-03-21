c6525d6a761a7b86205f65b267ce6af55ef5744c
rajavgs <113237654+rajavgs@users.noreply.github.com>
2023-03-21 00:13:36 -0700

    HTML/CSS Project

---
+++ header.js
@@ -0,0 +1,11 @@
+function navigateToHome(){
+    window.open("home.html","_top");
+}
+
+function navigateToAbout(){
+    window.open("about.html","_top");
+}
+
+function navigateToRules(){
+    window.open("rules.html","_top");
+}
